e6b4c81116b5fedf02e3a7df14be00b08172cfec
OSCARGT94 <oscarfernan94@gmail.com>
2024-11-14 17:56:57 +0100

    inicio semaforo

---
+++ Assets/Scripts/Semaforo.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Semaforo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void ejemploSemaforo()
+    {
+        Debug.Log("Verde");
+        Debug.Log("Amarillo");
+        Debug.Log("Rojo");
+    }
+}
